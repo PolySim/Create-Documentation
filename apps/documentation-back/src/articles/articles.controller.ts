bc7bac53fa164086b87640d9f643563ed049b0c6
Simon Desdevises <simon.desdevises@gmail.com>
2025-07-26 15:31:21 +0200

    add plate editor

---
+++ apps/documentation-back/src/articles/articles.controller.ts
@@ -0,0 +1,46 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { Article } from '../../generated/prisma';
+import { ArticlesService } from './articles.service';
+
+@Controller('articles')
+export class ArticlesController {
+  constructor(private readonly articlesService: ArticlesService) {}
+
+  @Get()
+  async findAll(): Promise<Article[]> {
+    return this.articlesService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string): Promise<Article | null> {
+    return this.articlesService.findOne(id);
+  }
+
+  @Post()
+  async create(
+    @Body() data: { title: string; content: string; userId: number },
+  ): Promise<Article> {
+    return this.articlesService.create(data);
+  }
+
+  @Put(':id')
+  async update(
+    @Param('id') id: string,
+    @Body() data: { title?: string; content?: string },
+  ): Promise<Article> {
+    return this.articlesService.update(id, data);
+  }
+
+  @Delete(':id')
+  async delete(@Param('id') id: string): Promise<Article> {
+    return this.articlesService.delete(id);
+  }
+}
---
+++ apps/documentation-back/src/articles/articles.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@nestjs/common';
+import { Article } from '../../generated/prisma';
+import { PrismaService } from '../prisma/prisma.service';
+
+@Injectable()
+export class ArticlesService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async findAll(): Promise<Article[]> {
+    return this.prisma.article.findMany();
+  }
+
+  async findOne(id: string): Promise<Article | null> {
+    return this.prisma.article.findUnique({ where: { id } });
+  }
+
+  async create(data: {
+    title: string;
+    content: string;
+    userId: number;
+  }): Promise<Article> {
+    return this.prisma.article.create({ data });
+  }
+
+  async update(
+    id: string,
+    data: { title?: string; content?: string },
+  ): Promise<Article> {
+    return this.prisma.article.update({ where: { id }, data });
+  }
+
+  async delete(id: string): Promise<Article> {
+    return this.prisma.article.delete({ where: { id } });
+  }
+}
